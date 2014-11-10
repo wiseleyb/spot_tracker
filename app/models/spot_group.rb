1d25a35ed442cbb44fe53dc95fc4540951bd9353
Ben Wiseley <ben.wiseley@causes.com>
2014-11-10 06:22:27 -0800

    Add support for multiple feeds
    This refactors the JS to SpotGroup and spot_feed.display to decide
    what is mapped.
    
    It also adds custom pins, has an early form of color/per/feed and adds
    pin clustering for better performance.
    
    I still need to figure out something better for colors. They need to be
    more contrasting but automated.

--- app/models/spot_group.rb
+++ app/models/spot_group.rb
@@ -1,3 +1,13 @@
 class SpotGroup < ActiveRecord::Base
   has_many :spot_feeds, dependent: :destroy
+
+  def to_arr_for_json
+    spot_feeds.displayable.map(&:to_arr_for_json).
+      each_with_index.map do |sf, idx|
+      {
+        color: SpotFeed::COLORS.reverse[idx],
+        messages: sf
+      }
+    end
+  end
 end
