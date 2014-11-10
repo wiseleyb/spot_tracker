c3c2a9590273362bf45a6fe60972e3d5a89513d0
Ben Wiseley <ben.wiseley@causes.com>
2014-11-10 07:45:10 -0800

    Add legend to map

--- app/models/spot_group.rb
+++ app/models/spot_group.rb
@@ -2,11 +2,11 @@ class SpotGroup < ActiveRecord::Base
   has_many :spot_feeds, dependent: :destroy
 
   def to_arr_for_json
-    spot_feeds.displayable.map(&:to_arr_for_json).
-      each_with_index.map do |sf, idx|
+    spot_feeds.displayable.each_with_index.map do |sf, idx|
       {
         color: SpotFeed::COLORS.reverse[idx],
-        messages: sf
+        messages: sf.to_arr_for_json,
+        name: sf.name
       }
     end
   end
